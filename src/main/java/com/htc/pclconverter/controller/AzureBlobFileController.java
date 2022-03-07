b00cca395b85a08f7267dab996094384d7bf9f98
kumar <85148531+kumar367316@users.noreply.github.com>
2022-03-07 19:03:49 +0530

    Update AzureBlobFileController.java
    add message api

--- src/main/java/com/htc/pclconverter/controller/AzureBlobFileController.java
+++ src/main/java/com/htc/pclconverter/controller/AzureBlobFileController.java
@@ -43,4 +43,9 @@ public class AzureBlobFileController {
 		 * "attachment; filename=\"" + file + "\"").body(resource);
 		 */
 	}
+	
+	@GetMapping("/message")
+	public String message(){
+		return "Congrats ! your application deployed successfully in Azure Platform. !";
+	}
 }
